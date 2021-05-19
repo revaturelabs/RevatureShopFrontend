9729eb22fbee31bda0f45168e406218f9a2815c2
Dante Pizzini <pizzini@wisc.edu>
2021-05-19 16:21:56 -0600

    forgot with last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import { CartComponent } from './cart/cart.component';
+import {LoginComponent} from "./login/login.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        CartComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule
