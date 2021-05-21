493f919899c409bd7066a38a1fac29b4e0e39dd1
Ben30000 <47725693+Ben30000@users.noreply.github.com>
2021-05-20 22:01:31 -0500

    commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { UserInventoryPageComponent } from './user-inventory-page/user-inventory-page.component';
 import { AdminInventoryPageComponent } from './admin-inventory-page/admin-inventory-page.component';
 
+
 @NgModule({
     declarations: [
         AppComponent,
         UserInventoryPageComponent,
         AdminInventoryPageComponent
+        
     ],
     imports: [
         BrowserModule,
--- src/app/user-inventory-page/user-inventory-page.component.ts
+++ src/app/user-inventory-page/user-inventory-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InventoryItemsService } from '../INVENTORY_ITEMS_SERVICE/inventory-items.service';
 
 @Component({
   selector: 'app-user-inventory-page',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserInventoryPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _inventoryItemsService : InventoryItemsService) { }
 
   ngOnInit(): void {
+
+  }
+
+  get inventoryItemsService() {
+    return this._inventoryItemsService;
   }
 
 }
