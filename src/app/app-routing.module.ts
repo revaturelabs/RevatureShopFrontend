60b052bb868a75dddf8e4768f19afb1cfbb9c6da
Coach-BWise <wisebry11@gmail.com>
2021-05-24 15:36:04 -0400

    fixed the global font style issue. Recreated Login with bootstrap 5. Forced Material Components to inherit revature styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {UserInventoryPageComponent} from './components/user-inventory-page/user-inventory-page.component';
 import {LoginComponent} from "./components/login/login.component";
+import {CheckoutComponent} from "./components/checkout/checkout.component";
 
 
 const routes: Routes = [
     {path: 'category', component: UserInventoryPageComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'checkout', component: CheckoutComponent}
 ]
 
 @NgModule({
