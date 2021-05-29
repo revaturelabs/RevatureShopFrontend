f1e40a4faa4a634b25bdacec29ba67f78066dcb7
J-Sigma <jsasser143801@gmail.com>
2021-05-29 16:25:27 -0500

    User can now add and remove items from the cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {HttpService} from "../../services/http.service";
+import { HttpCartService } from "./http-cart.service";
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+    cartItems: any[];
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient, private httpCartService: HttpCartService) {
+      this.cartItems = [];
   }
 
+  ngOnInit(): void {
+      this.httpCartService.getCart('parkert77@gmail.com').subscribe((cart) => {
+          Object.keys(cart.stockItemMap).forEach((itemName) => {
+              this.httpCartService.getItemByName(itemName).subscribe((item) => {
+                  console.log(item);
+                  this.cartItems.push(item);
+              })
+          });
+      });
+  }
+
+    getItemImage(itemName: string): string{
+        if (itemName.includes("Hat")) {
+            return "../assets/images/revitup_hat.png";
+        }
+        else if (itemName.includes("Like A Boss")) {
+            return "../assets/images/codelikeaboss_t-shirt.png";
+        }
+        else if (itemName.includes("Socks")) {
+            return "../assets/images/socks_1.jpg";
+        }
+        return '';
+    }
+
+    removeCartItem(itemName: string): void {
+      console.log('removeCartItem');
+      this.httpCartService.getItemByName(itemName).subscribe((item) => {
+          this.httpCartService.removeItemFromCart(item);
+      });
+    }
 }
--- src/app/components/item-modal/item-modal.component.ts
+++ src/app/components/item-modal/item-modal.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {HttpItemModalService} from "./http-item-modal.service";
 
 @Component({
   selector: 'app-item-modal',
@@ -6,15 +7,22 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./item-modal.component.css']
 })
 export class ItemModalComponent implements OnInit {
-  
+
     @Input() title: string = '';
     @Input() src: string = '';
     @Input() description: string = '';
     @Input() quantity: number = 0;
 
-  constructor() { }
+  constructor(private httpItemModalService: HttpItemModalService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  addToCart(): void {
+      this.httpItemModalService.getItemByName(this.title).subscribe((item) => {
+          this.httpItemModalService.addItemToCart(item, 'parkert77@gmail.com');
+      });
   }
 
 }
