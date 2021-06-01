46c22e13e0a4530b2195ef50e19b1c20366bf140
pkt77 <parkerkt77@gmail.com>
2021-06-01 11:48:16 -0400

    Change Order properties to match backend

--- src/app/components/admin-add-points/admin-add-points.component.ts
+++ src/app/components/admin-add-points/admin-add-points.component.ts
@@ -1,34 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AdminAddPointsService} from "../../services/admin-add-points.service";
 import {Order} from "../../services/user-page.service";
 import {HttpResponse} from "@angular/common/http";
 
 @Component({
-  selector: 'app-admin-add-points',
-  templateUrl: './admin-add-points.component.html',
-  styleUrls: ['./admin-add-points.component.css']
+    selector: 'app-admin-add-points',
+    templateUrl: './admin-add-points.component.html',
+    styleUrls: ['./admin-add-points.component.css']
 })
 export class AdminAddPointsComponent implements OnInit {
-
-    email :string = '';
+    email: string = '';
 
     // @ts-ignore
-    order : Order = {
-        description : '',
-        amount : "0",
+    order: Order = {
+        cause: '',
+        change: 0,
         date: new Date()
     };
     // @ts-ignore
-    resp : HttpResponse<string>;
+    resp: HttpResponse<string>;
 
-  constructor(public pointService : AdminAddPointsService) { }
+    constructor(public pointService: AdminAddPointsService) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  updatePoints() {
-      console.log(this.order);
-      this.pointService.modifyPoints(this.email,this.order).subscribe(data => console.log(data));
+    updatePoints() {
+        console.log(this.order);
+        this.pointService.modifyPoints(this.email, this.order).subscribe(data => console.log(data));
 
-  }
+    }
 }
--- src/app/services/user-page.service.ts
+++ src/app/services/user-page.service.ts
@@ -1,76 +1,68 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-
 import {AccountService} from "./account.service";
-import {getLocaleDateTimeFormat} from "@angular/common";
 
-export interface Order{
-    amount:string;
-    date:Date;
-    description: string;
+export interface Order {
+    change: number;
+    date: Date;
+    cause: string;
 }
 
-
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserPageService {
-
     // @ts-ignore
-    orderList : Order[] = []
+    orderList: Order[] = []
     // @ts-ignore
-    transactions : Observable<Order[]>;
+    transactions: Observable<Order[]>;
     // @ts-ignore
-    previousOrders : Order[];
-    commerceURL:string = 'http://localhost:9001/commercems/commerce';
-    accountURL:string = 'http://localhost:9001/commercems/commerce';
+    previousOrders: Order[];
+    commerceURL: string = 'http://localhost:9001/commercems/commerce';
+    accountURL: string = 'http://localhost:9001/commercems/commerce';
     email: string = '';
     // @ts-ignore
     temp1: Order[] = [
-        {amount:"-30", date: new Date('Fri, 01 Jan 1971 00:00:00 GMT'), description: ""},
-        {amount:"30", date: new Date(Date.now() - 1223), description: ""},
-        {amount:"30", date: new Date(Date.now() - 123123), description: ""},
-        {amount:"-40", date: new Date(Date.now() - 12123), description: ""},
-        {amount:"30", date: new Date(Date.now() - 12313123), description: ""},
-        {amount:"30", date: new Date(Date.now() - 3123123), description: ""},
+        {change: -30, date: new Date('Fri, 01 Jan 1971 00:00:00 GMT'), cause: ""},
+        {change: 30, date: new Date(Date.now() - 1223), cause: ""},
+        {change: 30, date: new Date(Date.now() - 123123), cause: ""},
+        {change: -40, date: new Date(Date.now() - 12123), cause: ""},
+        {change: 30, date: new Date(Date.now() - 12313123), cause: ""},
+        {change: 30, date: new Date(Date.now() - 3123123), cause: ""},
     ];
 
     // @ts-ignore
     temp2: Order[] = [
-        {amount:"30", date: new Date(), description: ""},
-        {amount:"30", date: new Date(), description: ""},
-        {amount:"300", date: new Date(), description: ""},
-        {amount:"3000", date: new Date(), description: ""},
-        {amount:"-3000", date: new Date(), description: ""},
-        {amount:"30", date: new Date(), description: ""},
+        {change: 30, date: new Date(), cause: ""},
+        {change: 30, date: new Date(), cause: ""},
+        {change: 300, date: new Date(), cause: ""},
+        {change: 3000, date: new Date(), cause: ""},
+        {change: -3000, date: new Date(), cause: ""},
+        {change: 30, date: new Date(), cause: ""},
     ];
 
+    constructor(private http: HttpClient, private accountService: AccountService) {
+    }
 
+    orders(): Order[] {
+        console.log(this.accountService.account);
+        // @ts-ignore
+        this.email = this.accountService.account.email;
+        // @ts-ignore
+        this.previousOrders = this.accountService.account?.pointHistory;
+        this.transactions = this.http.get<Order[]>(this.accountURL + '/myOrderHistory/' + this.email);
+        // @ts-ignore
+        //this.pointChanges = this.accountService.account.pointsHistory;
 
-  constructor(private http: HttpClient, private accountService : AccountService) { }
-
-   orders(): Order[]{
-      console.log(this.accountService.account);
-      // @ts-ignore
-       this.email = this.accountService.account.email;
-       // @ts-ignore
-       this.previousOrders = this.accountService.account?.pointHistory;
-       this.transactions = this.http.get<Order[]>(this.accountURL + '/myOrderHistory/' + this.email);
-       // @ts-ignore
-       //this.pointChanges = this.accountService.account.pointsHistory;
-
-       this.transactions.subscribe( data => this.orderList = data);
+        this.transactions.subscribe(data => this.orderList = data);
 
         //this.orderList.concat(this.previousOrders);
 
-       this.orderList = this.orderList.concat(this.temp1);
-       this.orderList = this.orderList.concat(this.temp2);
-       console.log(this.orderList);
+        this.orderList = this.orderList.concat(this.temp1);
+        this.orderList = this.orderList.concat(this.temp2);
+        console.log(this.orderList);
 
         return this.orderList;
     }
-
 }
-
-
