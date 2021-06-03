5d55f0e45e3c012ecb602acd2f771e6266ba03a3
Dante Pizzini <pizzini@wisc.edu>
2021-06-03 00:11:49 -0600

    updated endpoint

--- src/app/navbar/navbar-service.service.ts
+++ src/app/navbar/navbar-service.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class NavbarServiceService {
 
-  baseServerURL = "http://localhost:9001/inventoryms/";
+  baseServerURL = "http://" + window.location.hostname + ":9001/inventoryms/";
 
   categories: string[] = [];
   clothingCategories: string[] = [];
@@ -25,20 +25,20 @@ export class NavbarServiceService {
       let clothingItemFound : boolean = false;
 
       for (let t = 0; t < catS.length; t++) {
-        if (catS.includes("SHIRT") || catS.includes("SHIRTS") || 
-        catS.includes("PANT") || catS.includes("PANTS") || 
-        catS.includes("HAT") || catS.includes("HATS") || 
+        if (catS.includes("SHIRT") || catS.includes("SHIRTS") ||
+        catS.includes("PANT") || catS.includes("PANTS") ||
+        catS.includes("HAT") || catS.includes("HATS") ||
         catS.includes("TANK-TOPS") || catS.includes("TANK-TOPS") ||
         catS.includes("SWEATER") || catS.includes("SWEATERS") ||
-        catS.includes("GLOVE") || catS.includes("GLOVS") || 
-        catS.includes("SOCK") || catS.includes("SOCs") || 
-        catS.includes("MITTEN") || catS.includes("MITTEs") || 
-        catS.includes("UNDERWEAR") || catS.includes("UNDERWEArs") || 
+        catS.includes("GLOVE") || catS.includes("GLOVS") ||
+        catS.includes("SOCK") || catS.includes("SOCs") ||
+        catS.includes("MITTEN") || catS.includes("MITTEs") ||
+        catS.includes("UNDERWEAR") || catS.includes("UNDERWEArs") ||
         catS.includes("BOXER") || catS.includes("BOXERS") ||
-        catS.includes("TIE") || catS.includes("TIES") || 
-        catS.includes("HAT") || catS.includes("HATS") || 
-        catS.includes("SCARF") || catS.includes("SCARVES") || 
-        catS.includes("JACKET") || catS.includes("JACKETS") || 
+        catS.includes("TIE") || catS.includes("TIES") ||
+        catS.includes("HAT") || catS.includes("HATS") ||
+        catS.includes("SCARF") || catS.includes("SCARVES") ||
+        catS.includes("JACKET") || catS.includes("JACKETS") ||
         catS.includes("SHOE") || catS.includes("SHOES")
         ) {
               this.clothingCategories.push(this.categories[i]);
@@ -50,7 +50,7 @@ export class NavbarServiceService {
         this.accessoryCategories.push(this.categories[i]);
       }
     }
-  
+
   }
   getAllCategories(): Observable<string[]> {
     this.categories = [];
@@ -60,6 +60,6 @@ export class NavbarServiceService {
   }
 
 
-  
+
 
 }
